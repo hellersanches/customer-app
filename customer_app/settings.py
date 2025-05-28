1dbdf29f05138a80838edf3450cb8fea88b04496
Heller Sanchez <hellersanches@gmail.com>
2025-05-29 00:47:28 +0100

    Add Render domain to ALLOWED_HOSTS

--- customer_app/settings.py
+++ customer_app/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'replace-me'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://customer-app-6nkj.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
